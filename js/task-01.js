e141c917efc335dd364b7f847caea0d98935399c
DariaYashchuk <dariayashchuk2000@gmail.com>
2023-04-19 15:19:58 +0300

    19.05

--- js/task-01.js
+++ js/task-01.js
@@ -8,6 +8,6 @@ console.log("Number of categories: ", lengthOfCategories);
 // 2 part
 
 categories.forEach(function (element) {
-  console.log(`Category:`, element.querySelector("h2").textContent);
-  console.log(`Elements:`, element.querySelectorAll("li").length);
+  console.log(`Category:`, element.firstElementChild.textContent);
+  console.log(`Elements:`, element.lastElementChild.children.length);
 });
--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,7 @@
 const inputValue = document.querySelector("#font-size-control");
 const textContent = document.querySelector("#text");
 
+textContent.style.fontSize = inputValue.value + "px";
 inputValue.addEventListener("input", onInputChange);
 
 function onInputChange(event) {
